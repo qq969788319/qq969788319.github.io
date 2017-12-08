e2fce84e474feaf2402bb7f0c9619f98cd22472f
qq969788319 <969788319@qq.com>
2017-12-08 11:24:47 +0800

    同步

--- weichangjing/swiper2/js/script.js
+++ weichangjing/swiper2/js/script.js
@@ -8,5 +8,14 @@ var swiper = new Swiper('.swiper-container', {
         el: '.swiper-pagination'
         , //设置小圆点可以点击
         clickable: true
-    , }
+    , },
+    on:{
+      init: function(){
+        swiperAnimateCache(this); //隐藏动画元素 
+        swiperAnimate(this); //初始化完成开始动画
+      }, 
+      slideChangeTransitionEnd: function(){ 
+        swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+      } 
+    }
 , });
